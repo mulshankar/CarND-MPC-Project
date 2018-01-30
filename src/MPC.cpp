85209b23c9133997a5c1a0aaba37c497f42a91ec
Sankar Rengarajan <mulshankar@gmail.com>
2018-01-30 07:36:04 -0600

    some updates

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -224,8 +224,17 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
 
 	auto cost = solution.obj_value;
 	std::cout << "Cost " << cost << std::endl;
-	return {solution.x[x_start + 1],   solution.x[y_start + 1],
-		  solution.x[psi_start + 1], solution.x[v_start + 1],
-		  solution.x[cte_start + 1], solution.x[epsi_start + 1],
-		  solution.x[delta_start],   solution.x[a_start]};
+	
+	vector<double> result;
+  
+	result.push_back(solution.x[delta_start]);
+	result.push_back(solution.x[a_start]);
+ 
+	for (int i=0; i < N-1; i++)
+	{
+		result.push_back(solution.x[x_start + i + 1]);
+		result.push_back(solution.x[y_start + i + 1]);
+	 
+	}
+	return result;
 }
