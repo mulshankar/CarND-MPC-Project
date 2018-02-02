a230b84401cdd3264cfa0606fd592c1d7d325ce6
Sankar Rengarajan <mulshankar@gmail.com>
2018-02-02 11:10:11 -0600

    tune

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -45,9 +45,9 @@ class FG_eval {
 	
 		// The part of the cost based on the reference state.
 		for (int t = 0; t < N; t++) {
-		  fg[0] += 80*CppAD::pow(vars[cte_start + t]-cte_des, 2);
-		  fg[0] += 80*CppAD::pow(vars[epsi_start + t]-epsi_des, 2);
-		  fg[0] += 0.3*CppAD::pow(vars[v_start + t] - v_des, 2);
+		  fg[0] += 70*CppAD::pow(vars[cte_start + t]-cte_des, 2);
+		  fg[0] += 70*CppAD::pow(vars[epsi_start + t]-epsi_des, 2);
+		  fg[0] += 0.5*CppAD::pow(vars[v_start + t] - v_des, 2);
 		}
 
 		// Minimize the use of actuators.
